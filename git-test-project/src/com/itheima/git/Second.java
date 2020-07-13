e4effaebe3c0c61d86b8d04281348a6b09160164
ddd1one <2768410194@qq.com>
2020-07-13 12:29:34 +0800

    添加second

---
+++ git-test-project/src/com/itheima/git/Second.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+/**
+ * @author: dadada
+ * @date: Created in 2020-07-13 12:28
+ * @projectName:git-test-project
+ * @className:Second
+ * @description:
+ */
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("Second");
+    }
+}
