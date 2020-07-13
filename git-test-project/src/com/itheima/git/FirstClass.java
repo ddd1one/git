4386ef30f25db7a82cafcfc213bb9918a9d6dca0
ddd1one <2768410194@qq.com>
2020-07-13 11:29:58 +0800

    本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+/**
+ * @author: dadada
+ * @date: Created in 2020-07-13 11:20
+ * @projectName:git-test-project
+ * @className:FirstClass
+ * @description:
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+    }
+
+}
